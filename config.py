1563524e40f73a740fa287b5e37a623596c12cfc
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-08-01 02:27:16 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+from os import getenv
 
+API_ID = int(getenv("API_ID", "")) # get my.telegram.org/apps
+API_HASH = getenv("API_HASH", "") # get my.telegram.org/apps
+BOT_TOKEN = getenv("BOT_TOKEN", "") # get @botfather in telegram
