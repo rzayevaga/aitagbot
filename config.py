7db4bff9d336f9442173c2c51d219bdeab4056d6
Ağa Rzayev <rzayevaga.dev@gmail.com>
2025-08-14 11:16:18 +0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-from os import getenv
-
-API_ID = int(getenv("API_ID", "")) # get my.telegram.org/apps
-API_HASH = getenv("API_HASH", "") # get my.telegram.org/apps
-BOT_TOKEN = getenv("BOT_TOKEN", "") # get @botfather in telegram
